e7ecc5d60070ee47447290c7806a51e41a6e0253
Daniel Hensley <hensley.daniel@gmail.com>
2021-11-30 23:48:42 -0600

    Completed puzzle 1.

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod puzz1;
+
+fn main() {
+    println!("Hello, AoC is fun!\n");
+    println!("Puzzle 1");
+    println!("--------\n");
+    puzz1::puzz1();
+}
---
+++ src/puzz1.rs
@@ -0,0 +1,75 @@
+use std::error::Error;
+use std::path::Path;
+
+static INPUT_PATH: &str = "input/puzz1.csv";
+
+fn get_input(path: &Path) -> Result<Vec<i32>, Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_path(&path)?;
+    let mut input: Vec<i32> = Vec::new();
+    for result in rdr.records() {
+        input.push(result?.as_slice().parse::<i32>()?);
+    }
+    Ok(input)
+}
+
+fn diff(slc: &[i32]) -> Vec<i32> {
+    let slc_len = slc.len();
+    slc[0..slc_len - 1]
+        .iter()
+        .zip(slc[1..].iter())
+        .map(|(&v0, &v1)| v1 - v0)
+        .collect::<Vec<i32>>()
+}
+
+fn moving_sum(slc: &[i32], window_size: usize) -> Vec<i32> {
+    slc.windows(window_size)
+        .map(|w| w.iter().sum())
+        .collect::<Vec<i32>>()
+}
+
+fn count_positive(slc: &[i32]) -> u32 {
+    let mut cnt = 0;
+    for &v in slc.iter() {
+        if v > 0 {
+            cnt += 1;
+        }
+    }
+    cnt
+}
+
+pub(crate) fn puzz1() {
+    let input = get_input(Path::new(INPUT_PATH)).expect("Could not read input data");
+    let input_diff = diff(&input);
+    let cnt_larger = count_positive(&input_diff);
+    println!(
+        "{} measurements larger than the previous measurement",
+        cnt_larger
+    );
+    let window_summed_input = moving_sum(&input, 3);
+    let window_summed_diff = diff(&window_summed_input);
+    let cnt_larger = count_positive(&window_summed_diff);
+    println!(
+        "{} values larger than the previous value in window-summed input",
+        cnt_larger
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    static TEST_INPUT: [i32; 10] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+    #[test]
+    fn test_part_one() {
+        let count = count_positive(&diff(&TEST_INPUT));
+        assert_eq!(7, count);
+    }
+
+    #[test]
+    fn test_part_two() {
+        let count = count_positive(&diff(&moving_sum(&TEST_INPUT, 3)));
+        assert_eq!(5, count);
+    }
+}
